9b646afcc2d7493e1aa40e89c870cbb7018a4abd
Camilly Benato <camillybenato.dev2024@gmail.com>
2025-03-18 07:19:56 -0300

    Update parimpar.js

--- js/parimpar.js
+++ js/parimpar.js
@@ -2,8 +2,8 @@ function parimpar(){
     let n1 = parseFloat(document.getElementById("num1").value);
 
     if(n1 % 2 == 0){
-        document.getElementById("resultado").textContent = "Resultado = Par";
+        resultado.textContent = "Par";
     } else{
-        document.getElementById("resultado").textContent = "Resultado = Ímpar";
+        resultado.textContent = "Ímpar";
     }
 }
